73e6bf930f63b555c712ea88bc85c2c27a245f5f
Matt_qz <88900756+Matt-4-Code@users.noreply.github.com>
2023-11-25 08:58:12 -0300

    archivos descomprimidos

--- sketch.ino
+++ sketch.ino
@@ -1,22 +1,52 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#include <LiquidCrystal.h>
+int boton = 7;
+int lcdAddress = 0x27;
+LiquidCrystal_I2C lcd_1(lcdAddress, 16, 2);
+int contador = 4;
 
-int seconds = 0;
 
-LiquidCrystal lcd_1(12, 11, 5, 4, 3, 2);
+void setup () {
+  Wire.begin();
+  lcd_1.init();
+  pinMode(boton, INPUT);
+  lcd_1.backlight();
+  lcd_1.setCursor(0, 0);
 
-
-void setup() {
-  lcd_1.begin(16, 2);
-  lcd_1.print("Tocando Puerta");
 }
 
-void loop() {
-  lcd_1.setCursor(3, 1);
-  lcd_1.print("Espera: ");
-  lcd_1.setCursor(12, 1);
-  lcd_1.print(seconds);
+void loop () {
+  if (boton == HIGH) {
+    lcd_1.setCursor(0, 1);
+    lcd_1.print("Hola...");
+  }
+  
+}
+
+/*
+
+Contador de Timbre y de Tiempo de espera:
+Esto va en el void loop.
+
+lcd_1.setCursor(0, 1);
   delay(1000);
-  seconds += 1;
-  if 
-}
+  while (contador != -1) {
+    lcd_1.setCursor(0,1);
+    lcd_1.print(contador);
+    contador -= 1;
+    delay(1000);
+
+    if (contador == 0) {
+      lcd_1.setCursor(0, 0);
+      lcd_1.clear();
+      lcd_1.print("Espera...");
+      lcd_1.setCursor(0, 1);
+      for (contador = 0; contador >= 0; contador++) {
+        lcd_1.setCursor(0, 1);        
+        lcd_1.print(contador);
+        delay(1000);
+      }
+    }
+  }
+*/
